6c94e319cccac6a294d335da89abd3ff32e83f3c
GustavoAntunes-dev <gustavo.eng8@hotmail.com>
2023-04-18 15:17:17 -0300

    classe Position.java

---
+++ ProjetoChess/src/main/Program.java
@@ -0,0 +1,11 @@
+package main;
+
+import boardgame.Position;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        Position pos = new Position(3, 6);
+
+        System.out.println(pos);
+    }
+}

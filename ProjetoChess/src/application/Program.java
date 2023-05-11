a89757308fa1625af9149a33e36047651ca52b2b
humbertomcar <humbertomcarneiro2004@gmail.com>
2023-05-11 03:58:43 +0000

    correcao no nome da pasta BoardGame, metodo para limpar a tela ao clicar enter e try and catch na user interface para excessoes

--- ProjetoChess/src/application/Program.java
+++ ProjetoChess/src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import BoardGame.Board;
+import Chess.ChessException;
 import Chess.ChessMatch;
 import Chess.ChessPiece;
 import Chess.ChessPosition;
@@ -14,17 +16,27 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
 
         while(true){
-        UserInterface.printBoard(chessMatch.getPieces());
-        System.out.println();
-        System.out.println("Source: ");
-        ChessPosition source = UserInterface.readChessPosition(sc);
+            try{
+                UserInterface.clearScreen();
+                UserInterface.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.println("Source: ");
+                ChessPosition source = UserInterface.readChessPosition(sc);
 
-        System.out.println();
-        System.out.println("Target: ");
-        ChessPosition target = UserInterface.readChessPosition(sc);
+                System.out.println();
+                System.out.println("Target: ");
+                ChessPosition target = UserInterface.readChessPosition(sc);
 
-        ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }
+            catch(ChessException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
+            catch(InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
-
     }
 }

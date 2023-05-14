f8f2bb474a455230b0265b63e8b4a0d6f27f5b25
joseneto99 <jose.2019180360@unicap.br>
2023-05-14 23:18:07 +0000

    modified:   ProjetoChess/src/Chess/ChessMatch.java         modified:   ProjetoChess/src/Chess/ChessPiece.java

--- ProjetoChess/src/Chess/ChessPiece.java
+++ ProjetoChess/src/Chess/ChessPiece.java
@@ -6,6 +6,8 @@ import BoardGame.Position;
 
 public abstract class ChessPiece extends Piece {
         private Color color;
+        private int moveCount;
+
 
         public ChessPiece(Board board, Color color) {
             super(board);
@@ -14,6 +16,20 @@ public abstract class ChessPiece extends Piece {
 
         public Color getColor() {
             return color;
+        }
+
+        public int getMoveCount(){
+            return moveCount;
+        }
+
+        public void increaseMoveCount(){
+            moveCount++;
+
+        }
+        
+        public void decreaseMoveCount(){
+            moveCount--;
+
         }
 
         public ChessPosition getChessPosition(){

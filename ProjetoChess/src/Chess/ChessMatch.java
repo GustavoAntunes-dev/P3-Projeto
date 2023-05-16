e6b2bb34f81c6d4ee6dfe45fea83dbee293795e7
humbertomcar <humbertomcarneiro2004@gmail.com>
2023-05-16 02:09:33 +0000

    primeira parte da regra de roque

--- ProjetoChess/src/Chess/ChessMatch.java
+++ ProjetoChess/src/Chess/ChessMatch.java
@@ -197,7 +197,7 @@ public class ChessMatch {
         placeNewPiece('g', 1, new Horse(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
-        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('e', 1, new King(board, Color.WHITE, this));
         placeNewPiece('h', 1, new Tower(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -214,7 +214,7 @@ public class ChessMatch {
         placeNewPiece('g', 8, new Horse(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
-        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK, this));
         placeNewPiece('h', 8, new Tower(board, Color.BLACK));
         placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
         placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
--- ProjetoChess/src/Chess/ChessPieces/King.java
+++ ProjetoChess/src/Chess/ChessPieces/King.java
@@ -2,14 +2,17 @@ package Chess.ChessPieces;
 
 import BoardGame.Board;
 import BoardGame.Position;
+import Chess.ChessMatch;
 import Chess.ChessPiece;
 import Chess.Color;
 
 public class King extends ChessPiece {
 
-    public King(Board board, Color color) {
+    private ChessMatch chessMatch;
+
+    public King(Board board, Color color, ChessMatch chessMatch) {
         super(board, color);
-        
+        this.chessMatch = chessMatch;
     }
 
     @Override
@@ -20,6 +23,11 @@ public class King extends ChessPiece {
     private boolean canMove(Position position){
         ChessPiece p = (ChessPiece)getBoard().piece(position);   
         return p == null || p.getColor() != getColor();
+    }
+
+    private boolean testTowerCastling(Position position){
+        ChessPiece p = (ChessPiece)getBoard().piece(position);
+        return p != null && p instanceof Tower && p.getColor() == getColor() && p.getMoveCount() == 0;
     }
 
     @Override
@@ -65,7 +73,33 @@ public class King extends ChessPiece {
         p.setValues(position.getRow()+1, position.getColumn() +1 );
         if(getBoard().positionExists(p) && canMove(p)){
             mat[p.getRow()][p.getColumn()]= true;
+             //roque
+        if(getMoveCount() == 0 && !chessMatch.getCheck()){
+            //roque pequeno
+            Position posT1 = new Position(position.getRow(), position.getColumn() + 3);
+            
+            if(testTowerCastling(posT1)){
+                Position p1 = new Position(position.getRow(), position.getColumn() + 1);
+                Position p2 = new Position(position.getRow(), position.getColumn() + 2);
+                
+                if(getBoard().piece(p1) == null && getBoard().piece(p2) == null){
+                    mat[position.getRow()][position.getColumn() + 2] = true;
+                }
+            }
+            //roque grande
+            Position posT2 = new Position(position.getRow(), position.getColumn() - 4);
+            
+        if(testTowerCastling(posT2)){
+            Position p1 = new Position(position.getRow(), position.getColumn() - 1);
+            Position p2 = new Position(position.getRow(), position.getColumn() - 2);
+            Position p3 = new Position(position.getRow(), position.getColumn() - 3);
+            
+            if(getBoard().piece(p1) == null && getBoard().piece(p2) == null && getBoard().piece(p3) == null){
+                mat[position.getRow()][position.getColumn() - 2] = true;
+            }
         }
+    }
+}
         return mat;
     }
 }
